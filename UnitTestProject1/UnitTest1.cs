b17bdb74fa8b533a2e8116eb49ba4e89a963dee3
Karim <karimkuku@gmail.com>
2016-11-24 13:43:40 +0100

    unittest

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarsRover;
+
+namespace MarsRoverTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void RoverRunsIntoDirt()
+        {
+
+        }
+    }
+}
